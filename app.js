006e7c1a531678e2b74e3dc93aec760c9568ac62
Minko Todorov <kunobelin@yahoo.com>
2017-05-02 21:07:29 +0200

    created local strategy for passport

--- app.js
+++ app.js
@@ -41,7 +41,11 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(session({secret: 'books'}));
+app.use(session(
+    {secret: 'books',
+    resave: true,
+    saveUninitialized: true
+}));
 require('./src/config/passport')(app);
 
 app.set('views', './src/views');
