8617d3738e997b339a9609c97b6a1f444c5b0a64
Minko todorov <kunobelin@yahoo.com>
2017-05-02 18:49:29 +0200

    added mongodb

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require('express');
-
 var app = express();
+var sql = require('mssql');
+var config = {
+    user:'minko',
+    password: 'Parola1234',
+    server: 'nodejs.database.windows.net',
+    database: 'nodejs',
+
+    options: {
+        encrypt: true // for azure
+    }
+};
+
+// sql.connect(config, function(err){
+//     console.log(err);
+// })
 
 var port = process.env.PORT || 5000;
 var nav =
@@ -12,6 +26,7 @@ var nav =
 }];
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -20,6 +35,7 @@ app.set('view engine', 'ejs');
 
 
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function(req,res)
 {
