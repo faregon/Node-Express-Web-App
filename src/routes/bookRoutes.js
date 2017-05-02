3a7f108e7c7c0707894a8ccc40319333e839f3df
Minko todorov <kunobelin@yahoo.com>
2017-05-02 20:17:14 +0200

    showing books from db

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
+var ObjectId = require('mongodb').ObjectID;
 
 var router = function (nav)
 {
@@ -23,15 +23,15 @@ var router = function (nav)
                     });
                 }
             );
-          });
+            });
         });
 
     bookRouter.route('/:id')
     .get(function (req, res) {
-        var id = new objectId(req.params.id);
+        var id = new ObjectId(req.params.id);
         var url =
                     'mongodb://localhost:27017/booksApp';
-        mongodb.connect(url, function(err, db){
+        mongodb.connect(url, function(err, db) {
             var collection = db.collection('books');
 
             collection.findOne({_id: id},
